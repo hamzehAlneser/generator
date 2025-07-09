c79a6bfd27b75587d21e8970d40a8ef8dda6d596
Hamzeh AbdulKareem <hamzeh.abdulkareem@salma.ai>
2025-07-09 03:04:33 +0300

    finished index

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(cookieParser());
 
+app.get('/deploy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'deploy.html'));
+});
+
+app.get('/:page', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', req.params.page + '.html'));
+});
+
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
